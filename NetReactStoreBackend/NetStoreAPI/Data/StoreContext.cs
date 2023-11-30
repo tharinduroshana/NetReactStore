78c31e0d510e05fcd3c47112ac5451c04871622a
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-12-01 00:44:24 +0200

    Added MySQL support for backend.

---
+++ NetReactStoreBackend/NetStoreAPI/Data/StoreContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using NetStoreAPI.Entities;
+
+namespace NetStoreAPI.Data;
+
+public class StoreContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public StoreContext(DbContextOptions<StoreContext> dbContextOptions) : base(dbContextOptions)
+    {
+        try
+        {
+            var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+            if (databaseCreator != null)
+            {
+                if (!databaseCreator.CanConnect()) databaseCreator.Create();
+                
+                if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.Write(e);
+        }
+    }
+}
