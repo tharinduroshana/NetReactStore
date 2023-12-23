3b8854a83c74f345b601808e1f6e77eeb4764326
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-12-23 23:46:37 +0200

    Added Header component

--- net-react-store-frontend/src/app/layout/App.tsx
+++ net-react-store-frontend/src/app/layout/App.tsx
@@ -1,26 +1,35 @@
-import React, {useEffect, useState} from 'react';
-import '../../App.css';
-import {Product} from "../models/products";
+import React, { useEffect, useState } from "react";
+import "../../App.css";
+import { Product } from "../models/products";
 import Catalog from "../../features/catelog/Catalog";
+import Header from "./Header";
+import { Container, CssBaseline } from "@mui/material";
 
 const App = () => {
-    const [products, setProducts] = useState<Array<Product>>([]);
+  const [products, setProducts] = useState<Array<Product>>([]);
 
-    useEffect(() => {
-        fetchProducts();
-    }, []);
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-    const fetchProducts = async () => {
-        const response = await fetch("http://localhost:5198/api/products");
-        const results = await response.json();
-        setProducts(results);
-    }
+  const fetchProducts = async () => {
+    const response = await fetch("http://localhost:5198/api/products");
+    const results = await response.json();
+    setProducts(results);
+  };
 
-    return (
-        <>
-            <Catalog products={products} createProduct={() => console.log("test")} />
-        </>
-    );
-}
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog
+          products={products}
+          createProduct={() => console.log("test")}
+        />
+      </Container>
+    </>
+  );
+};
 
 export default App;
---
+++ net-react-store-frontend/src/app/layout/Header.tsx
@@ -0,0 +1,13 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">Net React Store</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
