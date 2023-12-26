9d97c1aac6adbaceb31c5b080ef6256113ad0228
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-12-26 15:44:45 +0200

    - Added Controller for products api. - Implemented mock APIs

---
+++ NetReactStoreBackend/NetStoreAPI/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetStoreAPI.Data;
+using NetStoreAPI.Entities;
+
+namespace NetStoreAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly StoreContext _context;
+    
+    public ProductsController(StoreContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        return await _context.Products.FindAsync(id);
+    }
+
+    [HttpPost]
+    public ActionResult<Product> CreateProduct()
+    {
+        var product = new Product
+        {
+            Name = "Samsung Galaxy S23",
+            Description = "Samsung Galaxy S23",
+            Brand = "Samsung",
+            PictureUrl = "/images/s23.jpeg",
+            Price = 20000,
+            QuantityInStock = 25,
+            Type = "Type A"
+        };
+        _context.Products.Add(product);
+        _context.SaveChanges();
+
+        return product;
+    }
+}
