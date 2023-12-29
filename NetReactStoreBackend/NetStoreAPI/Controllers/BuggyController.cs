337bb3b406eaaf8e6c53fb923c1a3e11bf9dbedf
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-12-29 22:22:52 +0200

    Added controller to test errors

---
+++ NetReactStoreBackend/NetStoreAPI/Controllers/BuggyController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetStoreAPI.Controllers;
+
+public class BuggyController : BaseApiController
+{
+    [HttpGet("not-found")]
+    public IActionResult GetNotFound()
+    {
+        return NotFound();
+    }
+    
+    [HttpGet("bad-request")]
+    public IActionResult GetBadRequest()
+    {
+        return BadRequest(new ProblemDetails { Title = "This is a bad request"});
+    }
+    
+    [HttpGet("unauthorized")]
+    public IActionResult GetUnauthorized()
+    {
+        return Unauthorized();
+    }
+    
+    [HttpGet("validation-error")]
+    public IActionResult GetValidationError()
+    {
+        ModelState.AddModelError("Error 1", "First Error");
+        ModelState.AddModelError("Error 2", "Second Error");
+        return ValidationProblem();
+    }
+    
+    [HttpGet("server-error")]
+    public IActionResult GetServerError()
+    {
+        throw new Exception("Exception has occured!");
+    }
+}
