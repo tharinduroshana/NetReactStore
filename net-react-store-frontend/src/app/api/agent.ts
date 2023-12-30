77270bee379c2726a5dc240750a3a12190ed73a4
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-12-31 01:09:22 +0200

    Handled validation errors

--- net-react-store-frontend/src/app/api/agent.ts
+++ net-react-store-frontend/src/app/api/agent.ts
@@ -12,7 +12,19 @@ axios.interceptors.response.use(
   (error: AxiosError) => {
     const { data, status } = error.response as AxiosResponse;
     switch (status) {
-      case 400:
+      case 400: {
+        if (data.errors) {
+          const modelStateErrors: string[] = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modelStateErrors.push(data.errors[key]);
+            }
+          }
+          throw modelStateErrors.flat();
+        }
+        toast.error(data.title);
+        break;
+      }
       case 401:
       case 404:
       case 500: {
