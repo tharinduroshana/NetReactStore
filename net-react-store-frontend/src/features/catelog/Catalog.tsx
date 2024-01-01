5a1bb1a142d6a67a6936c7c5a59df94216049991
Tharindu Ranaweera <ranaweerat@gmail.com>
2024-01-01 19:40:41 +0200

    Added Loading Component to catalog

--- net-react-store-frontend/src/features/catelog/Catalog.tsx
+++ net-react-store-frontend/src/features/catelog/Catalog.tsx
@@ -2,17 +2,26 @@ import React, { useEffect, useState } from "react";
 import { Product } from "../../app/models/products";
 import ProductList from "./ProductList";
 import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 
 const Catalog = () => {
   const [products, setProducts] = useState<Array<Product>>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchProducts();
   }, []);
 
   const fetchProducts = async () => {
-    agent.Catalog.list().then((products) => setProducts(products));
+    agent.Catalog.list()
+      .then((products) => setProducts(products))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
   };
+
+  if (loading) {
+    return <LoadingComponent message="Loading catalog" />;
+  }
 
   return (
     <>
