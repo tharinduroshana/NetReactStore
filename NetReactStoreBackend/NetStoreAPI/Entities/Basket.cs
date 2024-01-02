94d143ccb64b5ada4dc8cf58894c9cc422890414
Tharindu Ranaweera <ranaweerat@gmail.com>
2024-01-02 22:35:49 +0200

    Added Basket Entity

---
+++ NetReactStoreBackend/NetStoreAPI/Entities/Basket.cs
@@ -0,0 +1,38 @@
+namespace NetStoreAPI.Entities;
+
+public class Basket
+{
+    public int Id { get; set; }
+    public string BuyerId { get; set; }
+    public List<BasketItem> Items { get; set; } = new();
+
+    public void AddItem(Product product, int quantity)
+    {
+        if (Items.All( basketItem => basketItem.ProductId != product.Id ))
+        {
+            Items.Add(new BasketItem { Product = product, Quantity = quantity });
+        }
+
+        var existingItem = Items.FirstOrDefault(basketItem => basketItem.ProductId == product.Id);
+        if (existingItem != null)
+        {
+            existingItem.Quantity += quantity;
+        }
+    }
+
+    public void RemoveItem(Product product, int quantity)
+    {
+        var item = Items.FirstOrDefault(basketItem => basketItem.ProductId == product.Id);
+        
+        if (item == null) return;
+        
+        if (item.Quantity > quantity)
+        {
+            item.Quantity -= quantity;
+        }
+        else
+        {
+            Items.Remove(item);
+        }
+    }
+}
