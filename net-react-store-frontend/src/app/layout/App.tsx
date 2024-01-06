2bb52b5172d0c11bc319f567ccbe0757763af1c4
Tharindu Ranaweera <ranaweerat@gmail.com>
2024-01-06 22:12:44 +0200

    Added React context and initialised basket on app init

--- net-react-store-frontend/src/app/layout/App.tsx
+++ net-react-store-frontend/src/app/layout/App.tsx
@@ -6,12 +6,30 @@ import {
   CssBaseline,
   ThemeProvider,
 } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useStoreContext } from "../context/StoreContext";
+import { getCookie } from "../util/util";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 const App = () => {
+  const { setBasket } = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie("buyerId");
+    console.log(buyerId);
+    if (buyerId) {
+      agent.Basket.get()
+        .then((basket) => setBasket(basket))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false));
+    }
+  }, [setBasket]);
+
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? "dark" : "light";
 
@@ -21,6 +39,8 @@ const App = () => {
       background: { default: paletteType === "light" ? "#eaeaea" : "#121212" },
     },
   });
+
+  if (loading) return <LoadingComponent message="Initialising app..." />;
 
   return (
     <ThemeProvider theme={theme}>
