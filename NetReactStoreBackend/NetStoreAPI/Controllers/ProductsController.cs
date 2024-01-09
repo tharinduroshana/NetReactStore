162990560106b7eba83778e720336021af383c30
Tharindu Ranaweera <ranaweerat@gmail.com>
2024-01-09 21:16:44 +0200

    Added search functionality to the API

--- NetReactStoreBackend/NetStoreAPI/Controllers/ProductsController.cs
+++ NetReactStoreBackend/NetStoreAPI/Controllers/ProductsController.cs
@@ -16,9 +16,9 @@ public class ProductsController : BaseApiController
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<Product>>> GetProducts(string orderBy)
+    public async Task<ActionResult<List<Product>>> GetProducts(string orderBy, string searchTerm)
     {
-        var query =  _context.Products.Sort(orderBy).AsQueryable();
+        var query =  _context.Products.Sort(orderBy).Search(searchTerm).AsQueryable();
 
         return await query.ToListAsync();
     }
