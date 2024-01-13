af4dcc83e562d7ebe29f0c1d2bf7f29875e98689
Tharindu Ranaweera <ranaweerat@gmail.com>
2024-01-13 23:28:13 +0200

    - Added pagination to products. - Added params class

--- NetReactStoreBackend/NetStoreAPI/Controllers/ProductsController.cs
+++ NetReactStoreBackend/NetStoreAPI/Controllers/ProductsController.cs
@@ -1,8 +1,10 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NetStoreAPI.Data;
 using NetStoreAPI.Entities;
 using NetStoreAPI.Extensions;
+using NetStoreAPI.RequestHelpers;
 
 namespace NetStoreAPI.Controllers;
 
@@ -16,12 +18,19 @@ public class ProductsController : BaseApiController
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<Product>>> GetProducts(string orderBy, string searchTerm, string brands,
-        string types)
+    public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery]ProductParams productParams)
     {
-        var query = _context.Products.Sort(orderBy).Search(searchTerm).Filter(brands, types).AsQueryable();
+        var query = _context.Products
+            .Sort(productParams.OrderBy)
+            .Search(productParams.SearchTerm)
+            .Filter(productParams.Brands, productParams.Types)
+            .AsQueryable();
 
-        return await query.ToListAsync();
+        var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
+        
+        Response.AddPaginationHeader(products.MetaData);
+
+        return products;
     }
 
     [HttpGet("{id}")]
