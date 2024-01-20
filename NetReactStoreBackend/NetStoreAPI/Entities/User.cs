2a6c7650c34e50dcef0f8a3104570dc43ef07157
Tharindu Ranaweera <ranaweerat@gmail.com>
2024-01-20 22:12:23 +0200

    Added missing private key

--- NetReactStoreBackend/NetStoreAPI/Entities/User.cs
+++ NetReactStoreBackend/NetStoreAPI/Entities/User.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NetStoreAPI.Entities;
 
 public class User
 {
+    [Key]
     public string Username { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public byte[] PasswordHash { get; set; }
