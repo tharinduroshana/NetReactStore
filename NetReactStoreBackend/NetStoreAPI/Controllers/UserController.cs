4bcbd6874e5924247839cd377d4432cc937f7f03
Tharindu Ranaweera <ranaweerat@gmail.com>
2024-01-21 15:57:13 +0200

    Implemented User login API endpoint

--- NetReactStoreBackend/NetStoreAPI/Controllers/UserController.cs
+++ NetReactStoreBackend/NetStoreAPI/Controllers/UserController.cs
@@ -7,7 +7,7 @@ namespace NetStoreAPI.Controllers;
 public class UserController : BaseApiController
 {
     private readonly IUserService _userService;
-    
+
     public UserController(IUserService userService)
     {
         _userService = userService;
@@ -19,16 +19,28 @@ public class UserController : BaseApiController
         var result = await _userService.CreateUser(request);
         if (!result.Success)
         {
-            if (result.ErrorCode == 409)
-            {
-                return Conflict(result.ErrorMessage);
-            }
-            else
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, result.ErrorMessage);
-            }
+            return result.ErrorCode == 409
+                ? Conflict(result.ErrorMessage)
+                : StatusCode(StatusCodes.Status500InternalServerError, result.ErrorMessage);
         }
-        
+
         return Created("users/", "User Created Successfully!");
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult> Login(UserLoginDto request)
+    {
+        var result = await _userService.LoginUser(request);
+        if (!result.Success)
+        {
+            return result.ErrorCode switch
+            {
+                404 => NotFound(result.ErrorMessage),
+                401 => Unauthorized(result.ErrorMessage),
+                _ => StatusCode(StatusCodes.Status500InternalServerError, result.ErrorMessage)
+            };
+        }
+
+        return Ok(result.Data);
     }
 }
