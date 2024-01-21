185d7074da9a44fed4e50efc5ae5fa9353d7cac6
Tharindu Ranaweera <ranaweerat@gmail.com>
2024-01-21 13:06:34 +0200

    Added Dto to Sign up user

---
+++ NetReactStoreBackend/NetStoreAPI/DTOs/UserSignUpDto.cs
@@ -0,0 +1,8 @@
+namespace NetStoreAPI.DTOs;
+
+public class UserSignUpDto
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Name { get; set; }
+}
