53815c107095ba8c277277661f7f52b75d6ca180
Tharindu Ranaweera <ranaweerat@gmail.com>
2024-01-22 21:22:43 +0200

    - Added Swagger Authorize button. - Supported JWT authorization.

--- NetReactStoreBackend/NetStoreAPI/Program.cs
+++ NetReactStoreBackend/NetStoreAPI/Program.cs
@@ -1,6 +1,12 @@
+using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using NetStoreAPI.Data;
 using NetStoreAPI.Middleware;
+using NetStoreAPI.Services.Tokens;
 using NetStoreAPI.Services.Users;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,9 +15,53 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setup =>
+{
+    setup.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Net React Sore Backend API",
+        Description = "The Net React Store REST API",
+        Contact = new OpenApiContact
+        {
+            Name = "Tharindu Ranaweera",
+            Url = new Uri("https://www.linkedin.com/in/tharindu-roshana-ranaweera/")
+        }
+    });
+    var jwtSecurityScheme = new OpenApiSecurityScheme
+    {
+        BearerFormat = "JWT",
+        Name = "JWT Authentication",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        Description = "Insert the JWT token in the input box.",
+
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    setup.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+    setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { jwtSecurityScheme, Array.Empty<string>() } 
+    });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFilename);
+
+    if (File.Exists(xmlPath))
+    {
+        setup.IncludeXmlComments(xmlPath);
+    }
+});
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowClient",
@@ -25,7 +75,24 @@ builder.Services.AddCors(options =>
 builder.Services.AddDbContext<StoreContext>(o =>
     o.UseMySQL(builder.Configuration.GetConnectionString("Default")));
 
-builder.Services.AddAuthentication();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["AppSettings:Issuer"],
+        ValidAudience = builder.Configuration["AppSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["AppSettings:Token"])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
