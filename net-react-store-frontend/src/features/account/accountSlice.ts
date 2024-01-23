60afd20d26e559a366bf6b0cc07262fd65da0f9d
Tharindu Ranaweera <ranaweerat@gmail.com>
2024-01-23 17:19:27 +0200

    - Implemented accountSlice

---
+++ net-react-store-frontend/src/features/account/accountSlice.ts
@@ -0,0 +1,39 @@
+import { User } from "../../app/models/user";
+import { createAsyncThunk, createSlice, isAnyOf } from "@reduxjs/toolkit";
+import { FieldValues } from "react-hook-form";
+import agent from "../../app/api/agent";
+
+type AccountState = {
+  user: User | null;
+};
+
+const initialState: AccountState = {
+  user: null,
+};
+
+export const signInUser = createAsyncThunk<User, FieldValues>(
+  "account/signInUser",
+  async (data, thunkAPI) => {
+    try {
+      const user = await agent.Account.login(data);
+      localStorage.setItem("user", JSON.stringify(user));
+      return user;
+    } catch (e: any) {
+      return thunkAPI.rejectWithValue({ error: e.data });
+    }
+  },
+);
+
+export const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addMatcher(isAnyOf(signInUser.fulfilled), (state, action) => {
+      state.user = action.payload;
+    });
+    builder.addMatcher(isAnyOf(signInUser.rejected), (state, action) => {
+      console.log(action.payload);
+    });
+  },
+});
