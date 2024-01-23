c1bba2c8abf1da586edfe28dd1017ef5705ab885
Tharindu Ranaweera <ranaweerat@gmail.com>
2024-01-23 19:54:29 +0200

    - Added routes to Login and Register. - Added accountSlice to the store.

--- net-react-store-frontend/src/app/store/configureStore.ts
+++ net-react-store-frontend/src/app/store/configureStore.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { basketSlice } from "../../features/basket/basketSlice";
 import { catalogSlice } from "../../features/catelog/catalogSlice";
+import { accountSlice } from "../../features/account/accountSlice";
 
 export const store = configureStore({
   reducer: {
     basket: basketSlice.reducer,
     catalog: catalogSlice.reducer,
+    account: accountSlice.reducer,
   },
 });
 
