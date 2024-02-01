79858d325c6acb6eab1eb3f24d6438a00b5e3527
Tharindu Ranaweera <ranaweerat@gmail.com>
2024-02-01 18:55:13 +0200

    Added UserAddress to User entity

--- NetReactStoreBackend/NetStoreAPI/Entities/User.cs
+++ NetReactStoreBackend/NetStoreAPI/Entities/User.cs
@@ -9,4 +9,5 @@ public class User
     public string Name { get; set; } = string.Empty;
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
+    public UserAddress Address { get; set; }
 }
